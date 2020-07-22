b4faff66555cebe0db669bb657e103fbff638226
sagaringale520 <sagaringale520@gmail.com>
2020-07-22 22:32:01 +0530

    String Comparison added

---
+++ StringInPython/MemberShipOperator.py
@@ -0,0 +1,16 @@
+#Membership Operator in and not in
+
+name="sagar ingale"
+
+print('l' in name)
+print('k' in name)
+
+print('a' not in name)
+print('P' not in name)
+
+sub="sagar"
+
+if sub in name:
+    print("Sagar is present in Name")
+else:
+    print("Sagar is not Present in Name")
---
+++ StringInPython/StringComparision.py
@@ -0,0 +1,18 @@
+#String cAMPARISON IS BASED ON ALPHABETICAL Order
+
+name1="Sagar"
+name2="Karan"
+name3="sagar"
+name4="Sagar"
+
+print(name1==name3)
+print(name1==name4)
+print(name1!=name3)
+
+print=(name1>=name2)
+print=(name1<=name2)
+
+if name1 >= name2:
+    print(name1)
+else:
+    print(name2)
