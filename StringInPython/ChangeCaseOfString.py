aa8c10ab581ccafc7c9e8cf4415eec4a3d8bf91d
sagaringale520 <sagaringale520@gmail.com>
2020-07-23 10:22:26 +0530

    String Operation

---
+++ StringInPython/ChangeCaseOfString.py
@@ -0,0 +1,9 @@
+#Change the Case of the String
+
+str1="Python is an Programming language"
+
+print(str1.upper())
+print(str1.lower())
+print(str1.swapcase())
+print(str1.title())
+print(str1.capitalize())
---
+++ StringInPython/FormattingTheString.py
@@ -0,0 +1,9 @@
+#Formatting the String
+
+name="Sagar"
+age=26
+salary=10000
+
+print("{}'s Salary is {} and age is {}".format(name,salary,age))
+print("{0}'s Salary is {1} and age is {2}".format(name,salary,age))   #order
+print("{a}'s Salary is {b} and age is {c}".format(a=name,b=salary,c=age))     #variable
