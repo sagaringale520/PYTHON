b8987dccac30a6c3446f7fa9d7b96b16f4aa0527
sagaringale520 <sagaringale520@gmail.com>
2020-08-06 08:42:50 +0530

    ListFunctionOrMethod

---
+++ List/ListPopMethod.py
@@ -0,0 +1,5 @@
+#List pop method
+l1=[10,20,30,40]
+print(l1)
+l1.pop()
+print(l1)
---
+++ List/ListRemoveMethod.py
@@ -0,0 +1,11 @@
+#List Remove method
+
+l1=[10,20,30,40]
+print(l1)
+l1.remove(30)
+print(l1)
+
+#l1.remove(70)     #will give error
+
+print(l1.__contains__(20))
+print(l1.__contains__(50))
