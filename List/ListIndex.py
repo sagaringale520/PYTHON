c0e5ff66587d83cd6b3ea60f260a9c768275e61c
sagaringale520 <sagaringale520@gmail.com>
2020-08-06 07:08:05 +0530

    List Index

---
+++ List/ListIndex.py
@@ -0,0 +1,11 @@
+#List Index 0 to n
+
+l1=["Sagar","Ingale",26,20.5]
+print(l1)
+print(l1[1])
+#print(l1[4])     #Give Index error
+
+print(type(l1))
+print(type(l1[1]))
+print(type(l1[2]))
+print(type(l1[3]))
