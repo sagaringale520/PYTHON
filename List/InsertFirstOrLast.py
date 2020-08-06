df0e68c2b64759429eb1af6199f597d2721e22df
sagaringale520 <sagaringale520@gmail.com>
2020-08-06 08:31:43 +0530

    ListFunctionOrMethod

---
+++ List/InsertFirstOrLast.py
@@ -0,0 +1,5 @@
+#insert First or Last
+l1=[10,20,30,40]
+l1.insert(-100,"First")
+l1.insert(300,"Last")
+print(l1)
---
+++ List/ListExtendFunction.py
@@ -0,0 +1,12 @@
+#List Extend Function
+
+l1=[10,20,30]
+l2=[100,200,300]
+print(l1)
+print(l2)
+l2.extend(l1)
+print(l1)
+print(l2)
+print(type(l2))
+
+print("Total sum : ",sum(l2))
